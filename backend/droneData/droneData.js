4433530aff84b7d79dd8572acdf5a3448adf6099
bertcg <agonzalez9263@gmail.com>
2024-04-17 13:04:32 -0700

    fixed object creation

--- backend/droneData/droneData.js
+++ backend/droneData/droneData.js
@@ -18,7 +18,7 @@ function droneData(name, noiseLevel, range, endurance, maxAltitude) {
 
 const raven = new droneData("RQ-11 Raven", 70, 10, 90, 152);
 const shadow = new droneData("RQ-7 Shadow", 70, 109.5, 360, 3200);
-const puma = new droneData("RQ-20 Puma Raven", 70, 15, 120, 152);
+const puma = new droneData("RQ-20 Puma", 70, 15, 120, 152);
 const wasp = new droneData("RQ-12A Wasp III", 70, 5, 45, 300);
 
 drones.push(raven, shadow, puma, wasp);
