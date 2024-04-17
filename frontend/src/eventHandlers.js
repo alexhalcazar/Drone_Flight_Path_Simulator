e43642bb4f2b91f0ec88f9f1f8d84e152f45c2c5
bertcg <agonzalez9263@gmail.com>
2024-04-17 12:34:51 -0700

    added code for getting drone data

--- frontend/src/eventHandlers.js
+++ frontend/src/eventHandlers.js
@@ -1,6 +1,7 @@
 import {drone} from "../../backend/threejs/drone.js"
 import { cube2 } from "../../backend/threejs/threejsSetup.js";
 import { droneCoordPath } from "../../backend/threejs/measurePoints.js";
+import { drones } from "../../backend/droneData/droneData.js";
 export let ruler;
 
 document.querySelector('#btn-ruler-on').addEventListener('click', () => {
@@ -36,3 +37,27 @@ document.querySelector('#btn-move-drone').addEventListener('click', () => {
 document.querySelector('#btn-reset-drone').addEventListener('click', () => {
     drone.setCoords([-118.148512, 34.065868]);
 });
+
+document.querySelector('#drones-drop-down').addEventListener('change', () => {
+    const name = document.querySelector(".quantityDropdown select")
+        [document.querySelector(".quantityDropdown select").selectedIndex].innerHTML;
+
+    let noiseLevel;
+    let range;
+    let endurance;
+    let maxAltitude;
+
+    for (let i = 0; i < drones.length; i++) {
+        if (name === drones[i].name) {
+             noiseLevel = drones[i].noiseLevel;
+             range = drones[i].range;
+             endurance = drones[i].endurance;
+             maxAltitude = drones[i].maxAltitude;
+        }
+        console.log(noiseLevel, range, endurance, maxAltitude);
+    }
+    document.getElementById("meters").innerHTML = noiseLevel;
+    document.getElementById("dB").innerHTML = range;
+    document.getElementById("km").innerHTML = endurance;
+    document.getElementById("min").innerHTML = maxAltitude;
+})
